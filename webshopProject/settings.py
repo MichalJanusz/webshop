f2239176c0babfccb84e7b093f4b54c4f2d5ce8e
MichalJanusz <janusz.j.michal@gmail.com>
2020-10-10 12:59:37 +0200

    static files and __base__.html config

--- webshopProject/settings.py
+++ webshopProject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -74,6 +74,7 @@ WSGI_APPLICATION = 'webshopProject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+# database section moved to the bottom
 
 
 # Password validation
@@ -112,7 +113,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'webshop/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'webshop/static')
+
 
 try:
     from webshopProject.local_settings import DATABASES
